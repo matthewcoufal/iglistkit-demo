77a29178a8cf19e254d371f9ff4f2281e5f62d84
Matthew Coufal <mjc473@cornell.edu>
2019-03-01 14:03:49 -0500

    Initial Commit

---
+++ IGListKitDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IGListKitDemo
+//
+//  Created by Matthew Coufal on 3/1/19.
+//  Copyright © 2019 Two Tone Technologies LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
